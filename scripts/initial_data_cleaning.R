3f9467b3204be1524de01fc54d0185ca7f3cad15
chenrgengyi <chenrgengyi@gmail.com>
2026-04-03 14:49:25 +0800

    Update and rename initial_data_cleaning to initial_data_cleaning.R

--- scripts/initial_data_cleaning.R
+++ scripts/initial_data_cleaning.R
@@ -14,49 +14,30 @@ base_path <- "/Users/gengyichen/Desktop/CFCA/data/leopardfield/202601"
 print(dir_ls(base_path, type = "directory"))
 all_files <- dir_ls(base_path, recurse = TRUE)
 
-# 1. 将路径列表转化为表格
+# 1. path to table
 video_table <- tibble(full_path = as.character(all_files)) %>%
-  # 过滤掉非视频文件（以防万一）
   filter(str_detect(full_path, "(?i)\\.(mp4|avi|mov)$"))
 
-# 2. 核心解析逻辑
+# 2. 
 final_output <- video_table %>%
   mutate(
-    # 将路径按 "/" 拆分
     path_parts = str_split(full_path, "/"),
-    
-    # 提取相机号：倒数第3层 (A1)
     camera_id = map_chr(path_parts, ~ .x[length(.x) - 2]),
-    
-    # 提取物种名：倒数第2层 (家牛)
     species = map_chr(path_parts, ~ .x[length(.x) - 1]),
-    
-    # 提取文件名
     file_name = basename(full_path),
-    
-    # 从文件名提取日期时间 (正则匹配: 2026-01-03_11-03-09)
-    # \\d{4}-\\d{2}-\\d{2} 匹配日期，\\d{2}-\\d{2}-\\d{2} 匹配时间
     raw_ts = str_extract(file_name, "\\d{4}-\\d{2}-\\d{2}_\\d{2}-\\d{2}-\\d{2}"),
-    
-    # 转化为标准的 R 日期时间格式
-    # 注意：文件名里时间是用 "-" 隔开的，我们告诉 ymd_hms 函数
     shot_time = ymd_hms(raw_ts),
-    
-    # 拆分成你想要的格式
     date = as.Date(shot_time),
     time = format(shot_time, "%H:%M:%S")
   ) %>%
-  # 只保留你最终需要的四列
   select(camera_id, date, species, time) %>%
-  # 按相机和时间排序，方便检查
   arrange(camera_id, date, time)
 
 final_output <- final_output %>%
-  mutate(species = str_trim(species)) %>% # 去除看不见的空格
+  mutate(species = str_trim(species)) %>% # get rid of spaces
   mutate(species = case_when(
-    str_detect(species, "赤狐") ~ "赤狐", # 只要包含“赤狐”二字，全部统一为“赤狐”
+    str_detect(species, "赤狐") ~ "赤狐", # 赤狐 stand for red fox
     TRUE ~ species
   ))
 
-# 3. 检查前几行
 print(head(final_output))
